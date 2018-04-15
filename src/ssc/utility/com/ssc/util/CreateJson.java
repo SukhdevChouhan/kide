0399720bbed514e8c21cf848a6cc47ca899a7d6b
SukhdevChouhan <sukhdev_eck.com>
2018-04-16 01:55:31 +0530

    fixed jsonOnUI class

--- src/ssc/utility/com/ssc/util/CreateJson.java
+++ src/ssc/utility/com/ssc/util/CreateJson.java
@@ -17,20 +17,21 @@ import java.io.PrintWriter;
 public class CreateJson {
 	/**
 	 * @param args
-	 * @return 
+	 * @return
+	 * @return
 	 * @throws IOException
 	 */
-//	public static void main(String[] args) throws IOException {
-	public Map<String, Object> CreateJsonMethod() throws IOException{
+	// public static void main(String[] args) throws IOException {
+	public Map<String, Object> CreateJsonMethod() throws IOException {
 		String filePath = "E:/kida/kide/";
 		String fileName = "angularExcelapp01.xlsx";
 		int sheetNumber = 1;
 		GetExcelData data = new GetExcelData();
 		List<String> list = data.readHeader(filePath, fileName, sheetNumber);
-//		Map<Object,Object> returnArgs = new HashMap<Object, Object>();
+		// Map<Object,Object> returnArgs = new HashMap<Object, Object>();
 
 		// no need to convert header to map
-		 Map<String, Object> headerKeys = data.convertListToMap(list);
+		Map<String, Object> headerKeys = data.convertListToMap(list);
 		// System.out.println(headerKeys);
 
 		List<Object> excelDataList = data.readExcelData(filePath, fileName, sheetNumber);
@@ -38,44 +39,20 @@ public class CreateJson {
 		System.out.println("listOfValuesRowWise : " + listOfValuesRowWise);
 
 		CreateJson createJson = new CreateJson();
-		createJson.splitListData(listOfValuesRowWise);
-		return headerKeys;
+		return createJson.splitListData(listOfValuesRowWise);
 	}
 
-	public Object splitListData(List<List<Object>> listOfValuesRowWise) throws IOException {
-		Object[] splittedListData = null;
-		JSONObject dataObj = new JSONObject();
-		JSONArray dataArray = new JSONArray();
-
-		Object jsonValues = null;
-		List<Object> myArr = new ArrayList<Object>();
-		// System.out.println("Length : "+listOfValuesRowWise.size()+", subString : "+listOfValuesRowWise.get(0));
-		for (int i = 0; i < listOfValuesRowWise.size(); i++) {
-			Object sno = null;
-			myArr.clear();
-			dataObj.clear();
-			dataArray.clear();
-			// System.out.println(listOfValuesRowWise.get(i));
-			System.out.println("------------");
-			for (int j = 0; j < listOfValuesRowWise.get(i).size(); j++) {
-				sno = listOfValuesRowWise.get(i).get(0);
-				// System.out.println("each row sno : " + sno);
-				jsonValues = listOfValuesRowWise.get(i).get(j);
-				myArr.add(jsonValues);
-				// System.out.println("11 Values  :" + jsonValues);
+	public Map<String, Object> splitListData(List<List<Object>> listOfValuesRowWise) throws IOException {
+		Map<String, Object> myMap = new HashMap<String, Object>();
+		int i, j,k;
+		for (i = 0; i < listOfValuesRowWise.size(); i++) {
+			List<Object> listOfElevenValuesOfEachRow = new ArrayList<Object>(); 
+			for (j = 0; j < listOfValuesRowWise.get(i).size(); j++) {
+				listOfElevenValuesOfEachRow.add(listOfValuesRowWise.get(i).get(j));
 			}
-			dataObj.put(sno, myArr);
-			// dataArray.add(dataObj);
-			System.out.println(dataObj);
-			FileWriter file = new FileWriter("E:/kida/kide/test.json", true);
-			BufferedWriter bw = new BufferedWriter(file);
-			PrintWriter out = new PrintWriter(bw);
-			// out.println("{'intervals':[");
-			out.println(dataObj);
-			// out.println("]}");
-			out.close();
-		}
-		return splittedListData;
+			myMap.put((String) listOfValuesRowWise.get(i).get(0), listOfElevenValuesOfEachRow);
+			}
+		return myMap;
 	}
 
 }
